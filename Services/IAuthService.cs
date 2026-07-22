690185bd6bb473c37b9560e9dea93185a92360f0
D-4costa <danielleelisabeth01@gmail.com>
2026-07-22 05:53:21 -0500

    Refactor IAuthService methods to use Identity types

--- Services/IAuthService.cs
+++ Services/IAuthService.cs
@@ -1,14 +1,27 @@
 using AdoptionCenterDA.Web.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace AdoptionCenterDA.Web.Services;
 
 public interface IAuthService
 {
-    Task<bool> RegisterAsync(string fullName, string email, string password);
+    Task<IdentityResult> RegisterAsync(
+        string fullName,
+        string email,
+        string password);
 
-    Task<bool> LoginAsync(string email, string password);
+    Task<SignInResult> LoginAsync(
+        string email,
+        string password,
+        bool rememberMe = false);
 
     Task LogoutAsync();
 
     Task<ApplicationUser?> GetCurrentUserAsync();
+
+    Task<bool> IsInRoleAsync(
+        ApplicationUser user,
+        string role);
+
+    Task EnsureRolesCreatedAsync();
 }
