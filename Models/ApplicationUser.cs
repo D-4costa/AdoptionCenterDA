12ec4d03298a9a27989df9f3864ef23fa34d85ed
D-4costa <danielleelisabeth01@gmail.com>
2026-07-21 22:25:18 -0500

    Add ApplicationUser model with properties

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace AdoptionCenterDA.Web.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? FullName { get; set; }
+
+        public DateTime RegistrationDate { get; set; } = DateTime.Now;
+
+        public ICollection<AdoptionRequest>? AdoptionRequests { get; set; }
+    }
+}
