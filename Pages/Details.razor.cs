0dd32709e44c39a5e0932896d020fa639bb78ddc
D-4costa <danielleelisabeth01@gmail.com>
2026-07-22 06:01:14 -0500

    Update Details.razor.cs

--- Pages/Details.razor.cs
+++ Pages/Details.razor.cs
@@ -1,29 +1,30 @@
-using Microsoft.AspNetCore.Components;
 using AdoptionCenterDA.Web.Models;
 using AdoptionCenterDA.Web.Services;
+using Microsoft.AspNetCore.Components;
 
 namespace AdoptionCenterDA.Web.Components.Pages;
 
 public partial class Details : ComponentBase
 {
-
     [Parameter]
     public int Id { get; set; }
 
+    [Inject]
+    private IPetService PetService { get; set; } = default!;
 
     [Inject]
-    private PetService PetService { get; set; } = default!;
-
+    private IAuthService AuthService { get; set; } = default!;
 
     protected Pet? Pet { get; private set; }
 
+    protected bool IsAuthenticated { get; private set; }
 
-
-    protected override void OnInitialized()
+    protected override async Task OnInitializedAsync()
     {
-
         Pet = PetService.GetPetById(Id);
 
-    }
+        var user = await AuthService.GetCurrentUserAsync();
 
+        IsAuthenticated = user is not null;
+    }
 }
