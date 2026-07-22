968b367453673d058cd3a73d193a67e8c93b0e34
D-4costa <danielleelisabeth01@gmail.com>
2026-07-22 05:44:37 -0500

    Enhance authentication and database configuration

--- Program.cs
+++ Program.cs
@@ -1,40 +1,90 @@
 using AdoptionCenterDA.Web.Components;
+using AdoptionCenterDA.Web.Data;
+using AdoptionCenterDA.Web.Models;
 using AdoptionCenterDA.Web.Services;
+
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
-// Add Razor components
+// Database
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+// Identity
+
+builder.Services
+    .AddIdentity<ApplicationUser, IdentityRole>(options =>
+    {
+        options.Password.RequiredLength = 6;
+        options.Password.RequireDigit = true;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireNonAlphanumeric = false;
+
+        options.User.RequireUniqueEmail = true;
+    })
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+
+// Razor Components
+
 builder.Services
     .AddRazorComponents()
     .AddInteractiveServerComponents();
 
 
-// Register services
+// Authentication
+
+builder.Services.AddCascadingAuthenticationState();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = IdentityConstants.ApplicationScheme;
+})
+.AddIdentityCookies();
+
+builder.Services.AddAuthorization();
+
+
+// Services
+
 builder.Services.AddSingleton<IPetService, PetService>();
+
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 
 var app = builder.Build();
 
 
-// Configure HTTP request pipeline
+// Configure HTTP pipeline
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-
     app.UseHsts();
 }
-
 
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
-app.UseAntiforgery();
+app.UseRouting();
 
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
 
 app.Run();
